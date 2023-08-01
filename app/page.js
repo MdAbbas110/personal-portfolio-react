c59dd07bdcb11cf18957081d98ad4ca7a7d86ebe
Abbas <meerabbas110@gmail.com>
2023-08-01 11:21:15 +0530

    created the nav bar

--- app/page.js
+++ app/page.js
@@ -9,13 +9,13 @@ export default function Home() {
         <link rel='icon' href='./favicon.ico' />
       </Head>
 
-    <main>
+    <main className="px-10">
       <section className='min-h-screen'>
-        <nav>
-          <h1>Abbas Abidi</h1>
-          <ul>
-            <li><BsFillMoonStarsFill/></li>
-            <li><a href="#">Resume</a></li>
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-xl font-burtons">Abbas Abidi</h1>
+          <ul className="flex items-center">
+            <li className="cursor-pointer text-2xl"><BsFillMoonStarsFill/></li>
+            <li><a className="bg-gradient-to-r from-cyan-500 to-teal-500 px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
           </ul>
         </nav>
       </section>
